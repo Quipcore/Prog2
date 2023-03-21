775b910e7e67e0a9dcabaad7a13d731b6f46a3e0
Felix Lidö <felix.lido@gmail.com>
2023-03-21 18:25:19 +0100

    refactor of graph interface

--- src/main/java/com/main/Graph.java
+++ src/main/java/com/main/Graph.java
@@ -4,15 +4,15 @@ import java.util.List;
 import java.util.NoSuchElementException;
 
 public interface Graph <T> {
-    public void add(T node1, T node2);
-    public void remove(T node);
-    public void connect(T node1, T node2) throws NoSuchElementException,IllegalArgumentException,IllegalStateException;
-    public void disconnect(T node1, T node2, String edgeName, int weight) throws NoSuchElementException, IllegalStateException;
-    public void setConnectionWeight(Edge<T> edge, int weight) throws NoSuchElementException, IllegalArgumentException;
-    public T getNodes();
-    public Edge<T> getEdgeFrom(T node) throws NoSuchElementException;
-    public Edge<T> getEdgeBetween(T node1, T node2) throws NoSuchElementException;
-    public String toString();
-    public boolean pathExists(T node1, T node2);
-    public List<Edge<T>> getPath (T node1, T node2);
+    void add(T node1, T node2);
+    void remove(T node);
+    void connect(T node1, T node2) throws NoSuchElementException,IllegalArgumentException,IllegalStateException;
+    void disconnect(T node1, T node2, String edgeName, int weight) throws NoSuchElementException, IllegalStateException;
+    void setConnectionWeight(Edge<T> edge, int weight) throws NoSuchElementException, IllegalArgumentException;
+    T getNodes();
+    Edge<T> getEdgeFrom(T node) throws NoSuchElementException;
+    Edge<T> getEdgeBetween(T node1, T node2) throws NoSuchElementException;
+    String toString();
+    boolean pathExists(T node1, T node2);
+    List<Edge<T>> getPath (T node1, T node2);
 }
--- src/main/java/com/main/Main.java
+++ src/main/java/com/main/Main.java
@@ -16,5 +16,9 @@ public class Main extends Application {
         stage.show();
     }
 
-    public static void main(String[] args) {launch();}
+    public static void main(String[] args) {
+        ListGraph<Node> graph = new ListGraph<>();
+
+        launch();
+    }
 }
