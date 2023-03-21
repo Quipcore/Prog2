e325b71af7bba1d23c39d23bd4ca306cde6bb40f
Felix Lidö <felix.lido@gmail.com>
2023-03-21 11:31:54 +0100

    added test button

--- src/main/java/com/example/prog2/HelloController.java
+++ src/main/java/com/example/prog2/HelloController.java
@@ -4,11 +4,23 @@ import javafx.fxml.FXML;
 import javafx.scene.control.Label;
 
 public class HelloController {
+
+    boolean clicked = false;
+
     @FXML
     private Label welcomeText;
 
     @FXML
+    private Label textBox;
+    @FXML
     protected void onHelloButtonClick() {
         welcomeText.setText("Welcome to JavaFX Application!");
+    }
+
+    @FXML
+    protected void onClickMeButtonClick(){
+        String text = !clicked ? "Yay" : "Noooo";
+        textBox.setText(text);
+        clicked = !clicked;
     }
 }
