789aa750fd686704db8b5ab243e4a0b97dd279a0
Felix Lidö <felix.lido@gmail.com>
2023-03-27 13:02:58 +0200

    started using css for FXML

--- src/main/java/com/main/MainController.java
+++ src/main/java/com/main/MainController.java
@@ -1,10 +1,18 @@
 package com.main;
 
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 
 public class MainController {
 
+    @FXML
+    private Button loadButton;
+    @FXML
+    private Button saveButton;
+    @FXML
+    private Button exitButton;
     boolean clicked = false;
 
     @FXML
@@ -23,5 +31,19 @@ public class MainController {
         textBox.setText(text);
         clicked = !clicked;
         //textBox.setTranslateX(100); //<-- setTranslate to change pos
+    }
+
+    @FXML
+    protected void onExitButtonClick(){
+        exitButton.applyCss();
+        //System.exit(0);
+    }
+
+    @FXML
+    protected void onSaveButtonClick(ActionEvent actionEvent) {
+    }
+
+    @FXML
+    protected void OnLoadButtonClick(ActionEvent actionEvent) {
     }
 }
