680284d9195a9040b9761c2a86749293768c7f63
Felix Lidö <felix.lido@gmail.com>
2023-04-04 15:42:06 +0200

    fixed save/open

--- src/main/java/main/controllers/MainController.java
+++ src/main/java/main/controllers/MainController.java
@@ -167,7 +167,13 @@ public class MainController implements Controller {
     @FXML
     protected void onMenuNewMapClick(ActionEvent actionEvent) {
         InputStream mapStream = Objects.requireNonNull(getClass().getResourceAsStream("images/map.PNG"));
-        drawMap(new Image(mapStream));
+        Image image = new Image(mapStream);
+        outputArea.getChildren().clear();
+        imageView.setImage(image);
+        outputArea.getChildren().add(imageView);
+        stageManager.resizeStage();
+//        drawMap(new Image(mapStream));
+        graph = new ListGraph<>();
     }
 
     private void drawMap(Image mapImage){
@@ -179,7 +185,7 @@ public class MainController implements Controller {
     }
 
     @FXML
-    protected void onMenuOpenFileClick(ActionEvent actionEvent) throws IOException {
+    protected void onMenuOpenFileClick(ActionEvent actionEvent) throws IOException, InterruptedException {
         //Open and dislay data in selected .graph file
         // 1. Popup window to ask to continue and override unsaved changes
         // 2. Remove current data§
@@ -201,12 +207,56 @@ public class MainController implements Controller {
 //            addPinToMap(pin);
 //        }
 
+//        clearMap();
+
+        Path mapFilePath = Path.of("src/main/java/main/graph/saved/.graph");
+        List<String> fileList = Files.readAllLines(mapFilePath);
+
+        String[] pathSplit = fileList.get(0).split(":");
+        mapImage = new Image(new FileInputStream(pathSplit[1].trim()));
+        imageView.setImage(mapImage);
+        stageManager.resizeStage();
+
+        List<Pin> pins = new ArrayList<>();
+        String[] nodes = fileList.get(1).split(";");
+        for(int i = 0; i < nodes.length - 2; i += 3) {
+            Pin pin = new Pin(Double.parseDouble(nodes[i + 1]), Double.parseDouble(nodes[i + 2]), nodes[i]);
+            addPinToMap(pin);
+            pins.add(pin);
+        }
+
+        for(int i = 2; i < fileList.size();i++){
+            String[] connection = fileList.get(i).split(";");
+            Pin from = pins.stream().filter(pin -> pin.toString().equals(connection[0])).findFirst().get();
+            Pin to = pins.stream().filter(pin -> pin.toString().equals(connection[1])).findFirst().get();
+            String name = connection[2];
+            int weight = Integer.parseInt(connection[3]);
+            try{
+                addEdgeToMap(from, to, name, weight);
+            }catch (IllegalArgumentException e){
+                System.out.println("Weight must be a positive integer");
+            }catch (NoSuchElementException e){
+                System.out.println("Node does not exist");
+            }catch (IllegalStateException e){
+                //Expected exception because every connection is added twice
+                System.out.println("Connection already exists");
+            }
+        }
+
     }
 
-//    private void clearMap() {
+    private void clearMap() {
 //        outputArea.getChildren().clear();
-//        graph = new ListGraph<>();
-//    }
+        outputArea.getChildren().forEach(System.out::println);
+
+        outputArea.getChildren().clear();
+        imageView.setImage(null);
+        outputArea.getChildren().add(imageView);
+        stageManager.resizeStage();
+        outputArea.getChildren().forEach(System.out::println);
+
+        graph = new ListGraph<>();
+    }
 
     @FXML
     protected void onMenuSaveFileClick(ActionEvent actionEvent) throws IOException, URISyntaxException {
@@ -393,14 +443,7 @@ public class MainController implements Controller {
 
         dialog.getDialogPane().setContent(grid);
         dialog.setResultConverter(dialogButton -> new Pair<>(nameField.getText(), timeField.getText()));
-        dialog.showAndWait().ifPresent(result -> graph.connect(p0, p1, result.getKey(), Integer.parseInt(result.getValue())));
-
-        Line line = new Line();
-        line.setStartX(p0.getCircle().getCenterX());
-        line.setStartY(p0.getCircle().getCenterY());
-        line.setEndX(p1.getCircle().getCenterX());
-        line.setEndY(p1.getCircle().getCenterY());
-        outputArea.getChildren().add(line);
+        dialog.showAndWait().ifPresent(result -> addEdgeToMap(p0, p1, result.getKey(), Integer.parseInt(result.getValue())));
     }
 
     @FXML
@@ -467,8 +510,6 @@ public class MainController implements Controller {
             btnNewPlace.setDisable(false);
             return;
         }
-
-
     }
 
     private Pin getPinOnMousePos(MouseEvent mouseEvent) {
@@ -503,6 +544,17 @@ public class MainController implements Controller {
     private void addPinToMap(Pin pin) {
         outputArea.getChildren().add(pin.getCircle());
         graph.add(pin);
+    }
+
+    private void addEdgeToMap(Pin p0, Pin p1,String name, int weight) {
+        graph.connect(p0, p1, name, weight);
+
+        Line line = new Line();
+        line.setStartX(p0.getCircle().getCenterX());
+        line.setStartY(p0.getCircle().getCenterY());
+        line.setEndX(p1.getCircle().getCenterX());
+        line.setEndY(p1.getCircle().getCenterY());
+        outputArea.getChildren().add(line);
     }
 
     //-------------------- Utility Functions -------------
