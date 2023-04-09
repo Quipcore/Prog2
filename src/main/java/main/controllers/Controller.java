723debe3e3d163e1c28b58467683928f27925819
Felix Lidö <felix.lido@gmail.com>
2023-04-09 20:53:27 +0200

    removed comment

--- src/main/java/main/controllers/Controller.java
+++ src/main/java/main/controllers/Controller.java
@@ -2,7 +2,6 @@ package main.controllers;
 
 import main.StageManager;
 
-//Might want to split into controller and child controller interfaces.
 public interface Controller {
     void setStageManager(StageManager manager);
 }
